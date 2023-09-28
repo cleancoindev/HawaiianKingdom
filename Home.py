77cb0e763d3491f8e23591d0073f5f7c9bf87641
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2023-09-28 09:28:02 -1000

    Initial commit

---
+++ Home.py
@@ -0,0 +1,55 @@
+# Libraries
+import streamlit as st
+from PIL import Image
+
+# Confit
+st.set_page_config(page_title='TribeCalledRed TurtleShell Info Hub', page_icon=':bar_chart:', layout='wide')
+
+# Title
+st.title('Unofficial TribeCalledRed TurtleShell Info Hub')
+
+# Content
+c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15 = st.columns(15)
+c1.image(Image.open('images/ethereum-logo.png'))
+c2.image(Image.open('images/bsc-logo.png'))
+c3.image(Image.open('images/polygon-logo.png'))
+c4.image(Image.open('images/solana-logo.png'))
+c5.image(Image.open('images/avalanche-logo.png'))
+c6.image(Image.open('images/cosmos-logo.png'))
+c7.image(Image.open('images/near-logo.png'))
+c8.image(Image.open('images/flow-logo.png'))
+c9.image(Image.open('images/thorchain-logo.png'))
+c10.image(Image.open('images/osmosis-logo.png'))
+c11.image(Image.open('images/gnosis-logo.png'))
+c12.image(Image.open('images/optimism-logo.png'))
+c13.image(Image.open('images/arbitrum-logo.png'))
+c14.image(Image.open('images/axelar-logo.png'))
+
+c16, c17= st.columns(2)
+c16.image(Image.open('images/Screenshot - 8_21_2023 , 3_52_15 PM.png'))
+c17.image(Image.open('images/allenVans.png'))
+
+
+st.write(
+    """
+This Token project for Art and Video Game asset token NFTs is for native american alpha testers at this time only.  If you did not register to be part of this process you should not be here!
+    """
+)
+
+st.subheader('Methodology')
+st.write(
+    """
+    The data for this cross-chain comparison were selected via
+    data platform by using its **REST API**. These queries are currently set to **re-run every 24 hours** to cover the latest
+    data and are imported as a JSON file directly to each page. The data were selected with a **1 day delay** for all
+    blockchains to be in sync with one another.
+
+    It is worth mentioning that a considerable portion of the data used for this tool was manually decoded from the raw
+    transaction data on some of the blockchains. Besides that, the names of addresses, DEXs, collections, etc. are also
+    manually labeled. As the queries are updated on a daily basis to cover the most recent data, there is a chance
+    that viewers encounter inconsistent data through the app. Due to the heavy computational power required to execute
+    the queries, and also the size of the raw data being too large, it was not feasible to cover data for a longer period,
+    or by downloading the data and loading it from the repository itself. Therefore, the REST API was selected as the
+    proper form of loading data for the time being.
+    """
+)
---
+++ pages/1_👋_TribeCalledRed_Minting_Hub.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Halluci-Nation Info Hub",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Art Token TurtleShell Information Hub. 👋")
+
+st.markdown(
+    """
+    ### Want to learn more?
+    - This is an unofficial project for the Tribe Called Red, aka Halluci-Nation involving gamification of musical culture and retro game revival.
+    - This project is also a freedom project under the auspices of the EMIT-FREEDOM act.  More information will be provided in the community Slack Chat - Link TBA.
+    - We exist strictly to raise money for tribal concerns via crypto and musical fundraising and art economics in Games.
+
+
+"""
+)
