cdd67b0ba8c2d55d30c308f53b1b74654ee23e03
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2023-09-28 10:09:22 -1000

    Update 1_👋_Hawaiian_Nation_Minting_Hub.py

--- pages/1_👋_Hawaiian_Nation_Minting_Hub.py
+++ pages/1_👋_Hawaiian_Nation_Minting_Hub.py
@@ -10,7 +10,7 @@ st.write("# Hawaiian Nation Art Token and Fundrasing Info Hub. 👋")
 st.markdown(
     """
     ### Want to learn more?
-    - This is an unofficial project for the Tribe Called Red, aka Halluci-Nation involving gamification of musical culture and retro game revival.
+    - This is a project for the Hawaiian Nation and First People Tribes, involving gamification of musical culture and retro game revival.
     - This project is also a freedom project under the auspices of the EMIT-FREEDOM act.  More information will be provided in the community Slack Chat - Link TBA.
     - We exist strictly to raise money for tribal concerns via crypto and musical fundraising and art economics in Games.
 
